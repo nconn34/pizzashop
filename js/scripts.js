57b04891f51effa443c0b8d2cc4404c9ddd315a6
Nathan Conn <nconn34@gmail.com>
2021-12-11 12:45:50 -0800

    Tried to add full form reset functionality, was not able to figure it out, left code to pick up where I left off once I try and understand it better

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,6 @@ function Pizza(size, cheese, topping1, topping2, topping3){
     this.topping3=topping3;
 };
 
-console.log("PIZZA!!!!")
-
 Pizza.prototype.addTotal = function() {
   return this.size + this.cheese + this.topping1 + this.topping2 + this.topping3;
 };
@@ -28,6 +26,16 @@ $("#pizza-order").submit(function(event) {
     let price = newPizza.addTotal();
     let total = $("#total");
     total.empty();
-    total.append("<p> Total: " + price + "</p>")
+    if (price === 0){
+      total.append("<p> Total: " + "DID YOU EVEN READ THE FORM?!" + "</p>")
+    }  else if(price<=8) {
+      total.append("<p> Total: " + "Just a plain cheese slice? Bowser demands you live a little..." + "</p>")
+    } else {
+      total.append("<p> Total: $" + price + " ...Urghhhh...you only have human money? No coins? Hmmm...Bowser is confused, and must ask you to leave </p>")
+    }
+    $("#goaway").click(function() {
+        $("#total").hide("");
+        // $("#pizza-order").trigger(reset);
+    })
 })
 })
