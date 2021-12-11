4a96e7eb4bc6d4ed50d262e88a12bd58cb564bc4
Nathan Conn <nconn34@gmail.com>
2021-12-10 22:03:27 -0800

    Made edits to syntax

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,40 @@ function Pizza(size, cheese, topping1, topping2, topping3){
     this.size=size;
     this.cheese=cheese;
     this.topping1=topping1;
-    this.topping2 = topping2;
-    this.topping3 = topping3;
+    this.topping2=topping2;
+    this.topping3=topping3;
 };
 
-function Pizza(name, address){
-    this.name=name;
-    this.address=address
-};
+// function Customer(name, address){
+//     this.name=name;
+//     this.address=address
+// };
 
 console.log("PIZZA!!!!")
 
-      Pizza.prototype.addTotal = function(){
-          total = this.size + this.cheese + this.topping1 + this.topping2 + this.topping3
-          return total
-        }
-          
+Pizza.prototype.addTotal = function() {
+  return this.size + this.cheese + this.topping1 + this.topping2 + this.topping3;
+};
+
+        
+
+
+
+//User Interface
+
+$(document).ready(function(){
+$("#pizza-order").submit(function(event) {
+    event.preventDefault()
+    const size = parseInt($("#pizza-size").val());
+    const cheese = parseInt($("#cheese").val());
+    const topping1 = parseInt($("#topping1").val());
+    const topping2 = parseInt($("#topping2").val()); 
+    const topping3 = parseInt($("#topping3").val());
+    let newPizza = new Pizza(size, cheese, topping1, topping2, topping3);
+    let price = newPizza.addTotal();
+    console.log(price)
+    return price
+})
+})
+
+let newPizza = new Pizza(1, 1, 1, 1, 1);
