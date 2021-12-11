cdc2c228a8d35586fd1b1329f2163ad78a82d0b9
Nathan Conn <nconn34@gmail.com>
2021-12-10 16:35:13 -0800

    Wrote out specs for Customer constructor

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Code: function Pizza(size, cheese, topping1, topping2, topping3){
 };
 Expected Output: Will create a unique object for each Pizza order
 
+Describe: function Customer()
+Test: "It will create keys for each unique cutsomer placing an order, including name and delivery address"
+Code: function Pizza(name, address){
+    this.name=name;
+    this.address=address
+};
+Expected Output: Will create a unique object for each Customer placing an order
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //Business logic for Pizza orders
 
-function Pizza(size, cheese, topping1, topping2, topping3, name, address){
+function Pizza(size, cheese, topping1, topping2, topping3){
     this.size=size;
     this.cheese=cheese;
     this.topping1=topping1;
     this.topping2 = topping2;
     this.topping3 = topping3;
-    this.name = name;
-    this.address = address;
+};
+
+function Pizza(name, address){
+    this.name=name;
+    this.address=address
 };
