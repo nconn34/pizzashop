c86ea86a15e6ca8b1ea771f65d217744a2f7a787
Nathan Conn <nconn34@gmail.com>
2021-12-10 16:30:46 -0800

    Added pizza constructor to scripts file

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business logic for Pizza orders
+
+function Pizza(size, cheese, topping1, topping2, topping3, name, address){
+    this.size=size;
+    this.cheese=cheese;
+    this.topping1=topping1;
+    this.topping2 = topping2;
+    this.topping3 = topping3;
+    this.name = name;
+    this.address = address;
+};
